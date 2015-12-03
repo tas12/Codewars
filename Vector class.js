d7a603c39d9cfba3612951561271011b2839ab53
tas12 <tsultanah89@gmail.com>
2015-12-03 19:09:27 +0000

    Update Vector class.js

--- Vector class.js
+++ Vector class.js
@@ -1,52 +1,67 @@
 function Vector(vector){
-    
-  this.vector=vector;
-  }
-  
-  //add method
-  Vector.prototype.add= function(b){
-    var res=[];
-    var vec1 =this["vector"];
-    var vec2 = b["vector"];
-    for (i=0; i<vec1.length; i++){
-      res.push(vec1[i]+vec2[i]);
-    }
-    var result = new Vector(res);
-    return result;
-  };
-  //subtract method
-  Vector.prototype.subtract= function(b){
-    var res=[];
-    var vec1 =this["vector"];
-    var vec2 = b["vector"];
-    for (i=0; i<vec1.length; i++){
-      res.push(vec1[i]-vec2[i]);
-    }
-    var result = new Vector(res);
-    return result;
-  };
-  //dot product method
-  Vector.prototype.dot= function(b){
-    var res=0;
-    var vec1 =this["vector"];
-    var vec2 = b["vector"];
-    for (i=0; i<vec1.length; i++){
-      res+=vec1[i]*vec2[i];
-    }
-    return res;
-  };
-  //equals method
-  Vector.prototype.equals = function(answer){
-    if (this.toString()===answer.toString()){
-      return true;
-    } else {
-    console.log(this);
-        return false;
-    }
-  };
-//test
-var a = new Vector([1,2]);
-var b = new Vector([3,4]);
-a.add(b).equals(new Vector([4,6]));
-a.subtract(b).equals(new Vector([-2,-2]));
-a.dot(b).equals(11);
+
+      this.vector=vector;
+      }
+      //check method for comparing lengths of vectors
+      Vector.prototype.check = function(a,b){
+            if(a.length!==b.length){
+              throw new Error();
+          }
+      };
+      //add method
+      Vector.prototype.add= function(b){
+        var res=[];
+        var vec1 =this["vector"];
+        var vec2 = b["vector"];
+        this.check(vec1,vec2);
+        for (i=0; i<vec1.length; i++){
+          res.push(vec1[i]+vec2[i]);
+        }
+        var result = new Vector(res);
+        return result;
+      };
+      //subtract method
+      Vector.prototype.subtract= function(b){
+        var res=[];
+        var vec1 =this["vector"];
+        var vec2 = b["vector"];
+        this.check(vec1,vec2);
+        for (i=0; i<vec1.length; i++){
+          res.push(vec1[i]-vec2[i]);
+        }
+        var result = new Vector(res);
+        return result;
+          
+      };
+      //dot product method
+      Vector.prototype.dot= function(b){
+        var res=0;
+        var vec1 =this["vector"];
+        var vec2 = b["vector"];
+        this.check(vec1,vec2);
+        for (i=0; i<vec1.length; i++){
+          res+=vec1[i]*vec2[i];
+        }
+        return res;
+      };
+      //norm method
+      Vector.prototype.norm= function(){
+        var res=0;
+        for (i=0; i<this["vector"].length; i++){
+          res+=Math.pow(this["vector"][i],2);
+        }
+        return Math.sqrt(res);
+      };
+      //equals method
+      Vector.prototype.equals = function(answer){
+        if (this.toString()===answer.toString()){
+          return true;
+        } else {
+        console.log(this);
+            return false;
+        }
+      };
+      //toString method
+      Vector.prototype.toString = function(){
+          return "("+this["vector"].join()+")";
+      };
